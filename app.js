7dc60b9f65cc5c593267d61cc248096d01d1fbe3
lizsjh <37032480+lizsjh@users.noreply.github.com>
2020-10-11 15:19:51 -0400

    Update app.js

--- app.js
+++ app.js
@@ -144,5 +144,5 @@ botui.message.add({
 });
 
 function sendcomplete(){
-    window.parent.postMessage({"message": "completed","text":response}, "*");
+    window.parent.postMessage({"message": "completed","text":JSON.stringify(response)}, "*");
 };
